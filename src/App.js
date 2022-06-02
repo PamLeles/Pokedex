7b9781da0dac26b82aad742b44278c6f09617be3
PamLeles <pamela_leles_sousa@outlook.com>
2022-06-02 19:00:58 -0300

    using useEffect and useState to rederize, adding Async JS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import NavBar from './Components/NavBar';
 import SearchBar from './Components/SearchBar';
 import Pokedex from './Components/Pokedex';
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [pokemons, setPokemons] = useState([]);
+  const fetchPokemons = async () => {
+    try {
+      setLoading(true);
+    } catch (error) {
+      console.log('fetchPokemons error:', error);
+    }
+  };
+  useEffect(() => {
+    console.log('carregou!');
+    fetchPokemons();
+  });
   return (
     <div>
       <NavBar />
